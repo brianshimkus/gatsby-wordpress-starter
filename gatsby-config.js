0b314821c4ef64adba46ef96f02b9f28575e406a
Brian Shimkus <brianshimkus1@gmail.com>
2020-02-25 14:13:33 -0600

    Updated node file

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'demo.wp-api.org' or 'www.example-site.com'
          */
-        baseUrl: "http://gatsbywordpressstarter.local/",
+        baseUrl: "gatsbywordpressstarter.local",
         // The protocol. This can be http or https.
         protocol: "http",
         // Indicates whether the site is hosted on wordpress.com.
