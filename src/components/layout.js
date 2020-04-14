1db6c1afed2c90a374a91a075809eb85822a0206
Brian Shimkus <brianshimkus1@gmail.com>
2020-04-13 19:11:24 -0500

    Updated layout file

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,46 +6,29 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import MainMenu from "./MainMenu"
+import styled, { createGlobalStyle } from "styled-components"
 
-import Header from "./header"
-import "./layout.css"
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  body{
+    font-family: 'Open Sans', sans-serif;
+    margin: 0 !important;
+    padding: 0 !important;
+  }
+`
+const LayoutWrapper = styled.div`
+  max-width: 1200px;
+  margin 0 auto;
+`
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+const Layout = ({ children }) => (
+  <div>
+    <GlobalStyles />
+    <MainMenu />
+    <LayoutWrapper>{children}</LayoutWrapper>
+  </div>
+)
 
 export default Layout
